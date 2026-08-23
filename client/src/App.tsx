468aede568cd6b36e5cfb1d961d4460178e43953
Alex <alexescomail@gmail.com>
2026-08-23 12:26:22 -0400

    Load compact WNBA First Basket styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { queryClient } from "./lib/queryClient";
 import "./nrfi-premium.css";
 import "./best-plays-fullscreen.css";
 import "./wnba-tip-glow.css";
+import "./wnba-first-basket-compact.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
