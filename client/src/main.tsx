b5a855ab29e8c4ac95368f585304644dc99fc509
Alex <alexescomail@gmail.com>
2026-08-26 00:50:33 -0400

    Load WNBA arena backdrop fallbacks

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./mobile-nav-fixes.css";
+import "./wnba-arena-backdrops.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
